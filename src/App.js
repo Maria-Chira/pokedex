d168f18d6113133054fdd4b5090d0a8f39f94c7d
Maria Chira <chiramariageorgiana@gmail.com>
2021-11-14 16:44:30 +0000

    Exercises E and F completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 
+const Logo = () => {
+   return (<header>
+     <h1>Welcome to the Pokedex</h1>
+     <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="Pokedex Pokemon"></img>
+   </header>);
+};
+
+const BestPokemon = (props) => {
+ return <p>My favorite Pokemon is {props.name}</p>;
+};
+
 const App = () => {
   return (
     <div>
-      <header>
-        <h1>Welcome to the Pokedex</h1>
-        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="Pokedex Pokemon"></img>
-      </header>
+      <Logo />
+      <BestPokemon name="Squirtle" />
     </div>
   );
 };

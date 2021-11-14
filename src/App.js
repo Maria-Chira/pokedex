98bdf52e5ecc825e4158dece5c5fbbaac49f05cc
Maria Chira <chiramariageorgiana@gmail.com>
2021-11-14 19:02:49 +0000

    Exercise G completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React from "react";
 
 const Logo = () => {
-   return (<header>
-     <h1>Welcome to the Pokedex</h1>
-     <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="Pokedex Pokemon"></img>
-   </header>);
+  let appName = "CYF's Pokedex";
+   return (
+     <header>
+       <h1>Welcome to {appName}</h1>
+       <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="Pokedex Pokemon"></img>
+     </header>
+   );
 };
 
-const BestPokemon = (props) => {
- return <p>My favorite Pokemon is {props.name}</p>;
+
+
+const BestPokemon = () => {
+  let abilities = ["Anticipation", "Adaptability", "Run-Away"];
+  return (
+    <div>
+      <p>My favorite Pokemon is.</p>
+      <ul>
+        {abilities.map((ability) => (
+          <li>{ability}</li>
+        ))}
+      </ul>
+    </div>);
+  };
+
+const CaughtPokemon = () => {
+  let todayDate = new Date().toLocaleDateString();
+  return <p>Caught 0 Pokemon on {todayDate}</p>;
 };
 
 const App = () => {
   return (
     <div>
       <Logo />
-      <BestPokemon name="Squirtle" />
+      <BestPokemon />
+      <CaughtPokemon />
     </div>
   );
 };
